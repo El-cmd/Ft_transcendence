4d72c9e2d691b01997606adecebf7cb0b2ad41f4
Valentin LOTH <vloth@paul-f4Ar6s2.clusters.42paris.fr>
2025-01-01 20:20:07 +0100

    mvc html js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
-function toggleSection(sectionId) {
-	document.querySelectorAll('main section').forEach(section => {
-		section.classList.add('d-none');
-	});
-	document.getElementById(sectionId).classList.remove('d-none');
+document.addEventListener("DOMContentLoaded", function() {
+    loadHTML('partials/header.html', 'header-placeholder');
+    loadHTML('partials/sidebar.html', 'sidebar-placeholder');
+    loadHTML('partials/footer.html', 'footer-placeholder');
+	loadHTML('partials/login-modal.html', 'login-modal-placeholder');
+    loadSection('login'); // Charger la section par défaut
+});
+
+function loadHTML(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading HTML:', error));
+}
+
+function loadSection(section) {
+    const sectionMap = {
+        'friends': 'partials/friends.html',
+        'ranking': 'partials/ranking.html',
+        'chat': 'partials/chat.html',
+		'login': 'partials/login-modal.html',
+        'pong-game': 'partials/pong-game.html'
+    };
+    if (sectionMap[section]) {
+        loadHTML(sectionMap[section], 'main-content');
+    }
 }
