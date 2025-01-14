7bcd308ece214fc96df9c01cb86f46d5da514fa1
Valentin LOTH <vloth@paul-f5Ar6s1.clusters.42paris.fr>
2025-01-14 18:13:10 +0100

    Dockerfile

--- auth_service/jwtauth/jwtauth/settings.py
+++ auth_service/jwtauth/jwtauth/settings.py
@@ -90,10 +90,14 @@ WSGI_APPLICATION = 'jwtauth.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+      'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST', default='localhost'),
+           'PORT': config('DB_PORT', default='5432'),
+       }
 }
 
 
