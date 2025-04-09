e447d5f7f620df6172202aa9255202e43abbc512
Valentin Loth <58914622+El-cmd@users.noreply.github.com>
2025-04-09 08:33:02 +0200

    Delete back directory

--- back/db/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Substitute environment variables in init.sql
-envsubst < /docker-entrypoint-initdb.d/init.sql > /docker-entrypoint-initdb.d/init.sql.tmp
-mv /docker-entrypoint-initdb.d/init.sql.tmp /docker-entrypoint-initdb.d/init.sql
-
-# Run the original entrypoint script
-exec /usr/local/bin/docker-entrypoint.sh "$@"
